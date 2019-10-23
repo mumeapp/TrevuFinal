08d337f3169fe9ddd2c1d67e1482043ed092b9e9
yudhaananda <yudhaa.kres@gmail.com>
2019-10-23 22:34:24 +0700

    tambah banyak

--- app/src/main/java/com/remu/RestoranActivity.java
+++ app/src/main/java/com/remu/RestoranActivity.java
@@ -35,8 +35,11 @@ public class RestoranActivity extends AppCompatActivity {
     private final int PICK_IMAGE_REQUEST = 71;
     public static String kategori= "kategori";
     public static String Jenis = "jenis";
-    private EditText Namarestoran, AlamatRestoran, Deskripsi;
-    private Button Next;
+    public static String lat = "lat";
+    public static String lang="lang";
+    public static String nama= "nama";
+    private EditText Namarestoran, Deskripsi;
+    private Button Next, AlamatRestoran;
     private ImageView foto;
     private Uri filePath;
     private TextView Batal;
@@ -45,14 +48,12 @@ public class RestoranActivity extends AppCompatActivity {
     private UploadTask uploadTask;
     private DatabaseReference databaseReference;
     private String idUser;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halal_food_restaurant);
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
-
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         this.idUser = currentUser.getUid();
 
@@ -62,6 +63,8 @@ public class RestoranActivity extends AppCompatActivity {
         this.Next = findViewById(R.id.btn_Selanjutnya);
         this.Batal = findViewById(R.id.btn_batal);
         this.foto = findViewById(R.id.imageLogo);
+
+        AlamatRestoran.setOnClickListener(View -> setAlamat());
 
         foto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,50 +79,68 @@ public class RestoranActivity extends AppCompatActivity {
         Next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                StorageReference restoran = storageReference.child("Food").child("Restoran").child(getIntent().getStringExtra(Jenis));
-                uploadTask = restoran.putFile(filePath);
-                Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
-                    @Override
-                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(RestoranActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
+                try {
+                    StorageReference restoran = storageReference.child("Food").child("Restoran").child(getIntent().getStringExtra(Jenis));
+                    uploadTask = restoran.putFile(filePath);
+                    Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(RestoranActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
+                            }
+                            return restoran.getDownloadUrl();
                         }
-                        return restoran.getDownloadUrl();
-                    }
-                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Uri> task) {
-                        Intent get= getIntent();
-                        if (task.isSuccessful()) {
-                            Uri downloadUri = task.getResult();
-                            String id;
-                            String nama = Namarestoran.getText().toString();
-                            String alamat = AlamatRestoran.getText().toString();
-                            String deskripsi = Deskripsi.getText().toString();
-                            String foto1 = downloadUri.toString();
-                            String Kategori= get.getStringExtra(kategori);
-                            String jenis = get.getStringExtra(Jenis);
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            Intent get = getIntent();
+                            if (task.isSuccessful()) {
+                                Uri downloadUri = task.getResult();
+                                String id;
+                                String nama = Namarestoran.getText().toString();
+                                String alamat = get.getStringExtra(lat) + ", " + get.getStringExtra(lang);
+                                String deskripsi = Deskripsi.getText().toString();
+                                String foto1 = downloadUri.toString();
+                                String Kategori = get.getStringExtra(kategori);
+                                String jenis = get.getStringExtra(Jenis);
 
 
-                            Restoran restoran = new Restoran(nama, alamat, foto1, deskripsi);
-                            databaseReference = FirebaseDatabase.getInstance().getReference().child("Food").child("Restoran").child(jenis).child(Kategori).push();
-                            id = databaseReference.toString();
-                            databaseReference.setValue(restoran).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(RestoranActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                                Restoran restoran = new Restoran(nama, alamat, foto1, deskripsi);
+                                try{
+                                databaseReference = FirebaseDatabase.getInstance().getReference().child("Food").child("Restoran").child(jenis).child(Kategori).push();
+                                id = databaseReference.toString();
+                                databaseReference.setValue(restoran).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(RestoranActivity.this, "Success", Toast.LENGTH_SHORT).show();
 
+                                        }
                                     }
-                                }
-                            });
+                                });
 
+                            }catch (Exception e){
+                                    Toast.makeText(RestoranActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                            }
                         }
-                    }
-                });
+                    });
+                }catch (Exception e){
+                    Toast.makeText(RestoranActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
 
+    }
+
+    private void setAlamat(){
+        String jenis = getIntent().getStringExtra(Jenis);
+        String kate = getIntent().getStringExtra(kategori);
+        Intent in = new Intent(this, SetAddressActivity.class);
+        in.putExtra(SetAddressActivity.Jenis,jenis);
+        in.putExtra(SetAddressActivity.Kategori, kate);
+        startActivity(in);
     }
 
     @Override
---
+++ app/src/main/java/com/remu/SetAddressActivity.java
@@ -0,0 +1,67 @@
+package com.remu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.libraries.places.api.model.Place;
+import com.rtchagas.pingplacepicker.PingPlacePicker;
+
+public class SetAddressActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PLACE_PICKER = 1;
+    public static String Jenis= "jenis", Kategori= "kategori";
+    private String lat, lang, nama;
+    private String jenis, kategori;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_address);
+        jenis = getIntent().getStringExtra(Jenis);
+        kategori = getIntent().getStringExtra(Kategori);
+        showPlacePicker();
+    }
+    private void showPlacePicker() {
+        PingPlacePicker.IntentBuilder builder = new PingPlacePicker.IntentBuilder();
+        builder.setAndroidApiKey("AIzaSyA2yW_s0jqKnavh2AxISXB272VuSE56WI8")
+                .setMapsApiKey("AIzaSyA2yW_s0jqKnavh2AxISXB272VuSE56WI8");
+
+        // If you want to set a initial location rather then the current device location.
+        // NOTE: enable_nearby_search MUST be true.
+        // builder.setLatLng(new LatLng(37.4219999, -122.0862462))
+
+        try {
+            Intent placeIntent = builder.build(this);
+            startActivityForResult(placeIntent, REQUEST_PLACE_PICKER);
+        }
+        catch (Exception ex) {
+            // Google Play services is not available...
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == REQUEST_PLACE_PICKER) && (resultCode == RESULT_OK)) {
+            Place place = PingPlacePicker.getPlace(data);
+
+            if (place != null) {
+                lat = Double.toString(place.getLatLng().latitude);
+                lang = Double.toString(place.getLatLng().longitude);
+                nama = place.getName();
+                Toast.makeText(this, place.getAddress(), Toast.LENGTH_SHORT).show();
+                Intent in = new Intent(this, RestoranActivity.class);
+                in.putExtra(RestoranActivity.lat, lat);
+                in.putExtra(RestoranActivity.lang, lang);
+                in.putExtra(RestoranActivity.nama, nama);
+                in.putExtra(RestoranActivity.Jenis, jenis);
+                in.putExtra(RestoranActivity.kategori, kategori);
+                startActivity(in);
+            }
+        }
+    }
+
+}
