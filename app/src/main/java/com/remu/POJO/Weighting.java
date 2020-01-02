89976745720dca8555f0d7359f7f1302d41a36d5
yudhaananda <yudhaa.kres@gmail.com>
2020-01-02 08:14:24 +0700

    weighting halalBeverages & halalFood

--- app/src/main/java/com/remu/POJO/Weighting.java
+++ app/src/main/java/com/remu/POJO/Weighting.java
@@ -3,13 +3,15 @@ package com.remu.POJO;
 import java.util.ArrayList;
 
 public class Weighting {
-    private final double weightDistance = -0.3, weightGoogleRate = 0.167, weightTrevuRate = 0.25, weightIntencity = 0.25;
 
     public ArrayList<Double> doWeighting(double latitude, double longitude, ArrayList<PlaceModel> item) {
-        ArrayList<Double> s = new ArrayList<Double>();
+
+        double weightDistance = -0.3, weightGoogleRate = 0.167, weightTrevuRate = 0.25, weightIntencity = 0.25;
         double pembagi = 0;
         int count = 0;
-        ArrayList<Double> v = new ArrayList<Double>();
+        ArrayList<Double> s = new ArrayList<>();
+        ArrayList<Double> v = new ArrayList<>();
+
         for (PlaceModel place : item) {
             double lat = place.getPlaceLocation().latitude;
             double lng = place.getPlaceLocation().longitude;
@@ -20,9 +22,11 @@ public class Weighting {
             pembagi += s.get(count);
             count++;
         }
+
         for (Double vectorS : s) {
-            v.add(vectorS/pembagi);
+            v.add(vectorS / pembagi);
         }
+
         return v;
     }
 }
