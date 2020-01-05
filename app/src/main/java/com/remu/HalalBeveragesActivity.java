f9ad014c5dc5415671f28c3d2152736c9486a08f
yudhaananda <yudhaa.kres@gmail.com>
2020-01-05 20:29:43 +0700

    weighting

--- app/src/main/java/com/remu/HalalBeveragesActivity.java
+++ app/src/main/java/com/remu/HalalBeveragesActivity.java
@@ -355,14 +355,4 @@ public class HalalBeveragesActivity extends SlideBackActivity {
 
 }
 
-//class MyComparator implements Comparator<PlaceModel> {
-//    @Override
-//    public int compare(PlaceModel o1, PlaceModel o2) {
-//        if (o1.getPlaceWeight() > o2.getPlaceWeight()) {
-//            return -1;
-//        } else if (o1.getPlaceWeight() < o2.getPlaceWeight()) {
-//            return 1;
-//        }
-//        return 0;
-//    }
-//}
+
