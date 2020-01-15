8f1743517ddf36bdf7aec8957258608f9aaf6de7
frnkldgnwn <frank.gun@outlook.com>
2020-01-15 11:38:33 +0700

    nav_bar mantul

--- app/src/main/java/com/remu/MainActivity.java
+++ app/src/main/java/com/remu/MainActivity.java
@@ -1,25 +1,26 @@
 package com.remu;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
-import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.gauravk.bubblenavigation.BubbleNavigationConstraintView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private static final int ID_HOME = 1;
-    private static final int ID_SAVED = 2;
-    private static final int ID_FRIENDS = 3;
-    private static final int ID_PROFILE = 4;
+    private static final int ID_HOME = 0;
+    private static final int ID_SAVED = 1;
+    private static final int ID_FRIENDS = 2;
+    private static final int ID_PROFILE = 3;
 
-    private MeowBottomNavigation meowBottomNavigation;
     private NavController navController;
-    private int currentID;
+    private BubbleNavigationConstraintView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
         Animatoo.animateSlideLeft(this);
 
-        meowBottomNavigation = findViewById(R.id.nav_view);
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(ID_HOME, R.drawable.ic_home_black_24dp));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(ID_SAVED, R.drawable.ic_dashboard_black_24dp));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(ID_FRIENDS, R.drawable.ic_notifications_black_24dp));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(ID_PROFILE, R.drawable.ic_search_black_24dp));
-
-        meowBottomNavigation.show(1, true);
-        currentID = ID_HOME;
-
         navController = Navigation.findNavController(this, R.id.main_nav_host_fragment);
 
-        meowBottomNavigation.setOnClickMenuListener(model -> {
-            switch (model.getId()) {
+        navView = findViewById(R.id.nav_view);
+        navView.setTypeface(ResourcesCompat.getFont(this, R.font.geomanistregular));
+        navView.setNavigationChangeListener((view, position) -> {
+            switch (position) {
                 case ID_HOME:
                     navController.navigate(R.id.navigation_home);
                     break;
@@ -54,27 +48,6 @@ public class MainActivity extends AppCompatActivity {
                     navController.navigate(R.id.navigation_profile);
                     break;
             }
-            currentID = model.getId();
-            return null;
-        });
-
-        meowBottomNavigation.setOnShowListener(model -> {
-            switch (model.getId()) {
-                case ID_HOME:
-                    navController.navigate(R.id.navigation_home);
-                    break;
-                case ID_SAVED:
-                    navController.navigate(R.id.navigation_saved);
-                    break;
-                case ID_FRIENDS:
-                    navController.navigate(R.id.navigation_friends);
-                    break;
-                case ID_PROFILE:
-                    navController.navigate(R.id.navigation_profile);
-                    break;
-            }
-            currentID = model.getId();
-            return null;
         });
     }
 
@@ -82,10 +55,10 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
 
-        if (currentID == 1) {
+        if (navView.getCurrentActiveItemPosition() == ID_HOME) {
             finish();
         } else {
-            meowBottomNavigation.show(1, true);
+            navView.setCurrentActiveItem(ID_HOME);
         }
     }
 }
